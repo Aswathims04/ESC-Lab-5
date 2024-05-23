ab20f985d19b9cc207eaf2bb1eba3deed5572fdf
Aswathims04 <144255377+Aswathims04@users.noreply.github.com>
2024-05-23 20:36:29 +0530

    Update search_engin.py

--- search_engin.py
+++ search_engin.py
@@ -1,10 +1,8 @@
 import os
 from googleapiclient.discovery import build
-reacted1 = "'AIzaSyA47JPNTVMFf2nIoP2IYD0svgV0AJF7_sA'"
-reacted = "d3998bd1571bb41d9"
 
-API_KEY = reacted1
-CSE_ID = reacted
+API_KEY = ###reacted###
+CSE_ID = ###reacted###
 
 def google_search(search_term, api_key, cse_id, **kwargs):
     service = build("customsearch", "v1", developerKey=api_key)
